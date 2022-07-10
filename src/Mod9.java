08834c2f91a863074b0fc4242224e8fa17d7597b
Diego Monteiro <damonteiro89@gmail.com>
2022-07-09 21:12:56 -0300

    Conversao de valor primitivo em wrapper.

---
+++ src/Mod9.java
@@ -0,0 +1,10 @@
+public class Mod9 {
+    private int idade;
+
+    public static void main(String[] args) {
+        int idade = Integer.valueOf(12);
+        System.out.println(idade);
+
+    }
+
+}
